c26a15180aa96ab3db1f8e6557fa50eb5c91f5ae
eyal shay <eyalshay@eyals-MacBook-Air.local>
2022-09-07 15:53:09 +0100

    tidying up unused code

--- App.js
+++ App.js
@@ -1,30 +1,12 @@
-// import { StatusBar } from "expo-status-bar";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Scanner from "./components/Scanner";
-
-import { ScrollView, StyleSheet, View, Text } from "react-native";
-import { NativeRouter, Routes, Route } from "react-router-native";
-import Title from "./components/Title";
-import NavBar from "./components/Nav";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    // <NativeRouter>
-    //   <ScrollView>
-    //     <View style={styles.container}>
-    //       <StatusBar style="auto" />
-    //     </View>
-    //     <Title />
-    //     <NavBar />
-    //     <Routes>
-    //       <Route path="/" />
-    //     </Routes>
-    //   </ScrollView>
-    //       </NativeRouter>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
