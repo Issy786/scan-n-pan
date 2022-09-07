08875730630c55b85c6d219a2e6f8a702a29894d
eyal shay <eyalshay@eyals-MacBook-Air.local>
2022-09-07 09:31:41 +0100

    login working after solving the merge conflicts, home will need to move to new component

--- App.js
+++ App.js
@@ -1,23 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+import { ScrollView, StyleSheet, View, Text } from "react-native";
 import { NativeRouter, Routes, Route } from "react-router-native";
 import Title from "./components/title";
 import NavBar from "./components/nav";
 
 export default function App() {
   return (
-    <NativeRouter>
-      <ScrollView>
-        <View style={styles.container}>
-          <StatusBar style="auto" />
-        </View>
-        <Title />
-        <NavBar />
-        <Routes>
-          <Route path="/" />
-        </Routes>
-      </ScrollView>
-    </NativeRouter>
+    // <NativeRouter>
+    //   <ScrollView>
+    //     <View style={styles.container}>
+    //       <StatusBar style="auto" />
+    //     </View>
+    //     <Title />
+    //     <NavBar />
+    //     <Routes>
+    //       <Route path="/" />
+    //     </Routes>
+    //   </ScrollView>
+    //       </NativeRouter>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -30,3 +46,20 @@ const styles = StyleSheet.create({
     elevation: 7,
   },
 });
+
+const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           options={{ headerShown: false }}
+//           name="Login"
+//           component={Login}
+//         />
+//         <Stack.Screen name="Home" component={Home} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
